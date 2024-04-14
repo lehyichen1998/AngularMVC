52377c024ee3360992213e2e0f27c4152168e425
lehyichen1998 <lehyichen1998@gmail.com>
2024-04-14 11:03:48 +0800

    Add project files.

---
+++ AngularMVC/Models/EF/AngularMvcDbContent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace AngularMVC.Models.EF
+{
+    public class AngularMvcDbContent : DbContext
+    {
+        public AngularMvcDbContent():base("name = AngularMvcDbContent")
+        {
+
+        }
+
+        //setting EF convention
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Use singular table name
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
---
+++ AngularMVC/Models/EF/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AngularMVC.Models.EF
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(32)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Email { get; set; }
+        [StringLength(50,MinimumLength =2)]
+        public string Address { get; set; }
+    }
+}
